d4e2d1d99302280ae75ff35ac1bc3d893f6414ee
GEEG <e.glebov@innopolis.university>
2024-06-11 14:04:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const names = ["GEEG", "i-geeg-i", "Egor Glebov"];
+var nowName = 0; 
+setInterval(()=>{
+    nowName += 1;
+    if (nowName==names.length){
+        nowName=0;
+    }
+    document.getElementById("name").textContent = names[nowName];
+},2000)
