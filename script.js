c2aafcc35f504879815d958d5c83486c4d9a8ddd
GEEG <e.glebov@innopolis.university>
2024-06-12 19:41:20 +0300

    js minor update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const names = ["GEEG", "i-geeg-i", "Egor Glebov"];
 var nowName = 0;
-setInterval(()=>{
-    nowName += 1;
-    if (nowName==names.length){
-        nowName=0;
-    }
-    document.getElementById("name").textContent = names[nowName];
-},2000)
+document.addEventListener('DOMContentLoaded', function() {
+    setInterval(()=>{
+        nowName += 1;
+        if (nowName==names.length){
+            nowName=0;
+        }
+        document.getElementById("name").textContent = names[nowName];
+    },2000)
+});
